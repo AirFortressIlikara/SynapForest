0dff6ef03148db17d947a0a9f700a5297e31dfe3
ilikara <3435193369@qq.com>
2024-12-30 05:09:40 +0000

    db update

--- api/folder/folder.go
+++ api/folder/folder.go
@@ -2,7 +2,7 @@
  * @Author: ilikara 3435193369@qq.com
  * @Date: 2024-12-29 12:43:00
  * @LastEditors: ilikara 3435193369@qq.com
- * @LastEditTime: 2024-12-29 13:18:00
+ * @LastEditTime: 2024-12-29 15:00:49
  * @FilePath: /my_eagle/api/folder/folder.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
